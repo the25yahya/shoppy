f595ff114143c62dab0d87f06cc38e1d67466573
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-09-29 00:56:31 +0200

    adding initisl files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import './App.css'
+import React, {useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+
+
 
 function App() {
   return(
