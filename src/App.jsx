7e040fe21344cce8672ac909f5f101021235b5c8
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-10-01 01:56:04 +0200

    added tooltip component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import React, {useEffect} from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import avatar from './data/avatar.jpg'
 
 
 function App() {
   return(
-    <div></div>
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+         <div className='fixed right-4 bottom-4' style={{ zIndex:'1000' }}>
+          <TooltipComponent content="settings" position="top">
+           <button style={{background:"blue",borderRadius:"50%"}} type='button'
+           className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'>
+             <FiSettings />
+           </button>
+          </TooltipComponent>
+         </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
