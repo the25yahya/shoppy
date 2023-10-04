a90471cda6014b398c871c00ac8bfedc616187bf
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-10-04 16:15:10 +0200

    added route components

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,29 @@ function App() {
          </div>
          <div>
           <Routes>
-            <Route path='/'
-            element = "Ecommerce" />
+            {/*Dashboard*/}
+            <Route path='/' element = "Ecommerce" />
+            <Route path='/ecommerce' element = "Ecommerce" />
+            {/*Pages*/}
+            <Route path='/orders' element="Orders" />
+            <Route path='/employees' element="Employees" />
+            <Route path='/customers' element="Customers" />
+
+            {/*Apps*/}
+            <Route path='/kanban' element="Kanban" />
+            <Route path='/editor' element="Editor" />
+            <Route path='/calendar' element="Calendar" />
+            <Route path='/color-picker' element="ColorPicker" />
+
+            {/*Charts*/}
+            <Route path='/line' element="Line" />
+            <Route path='/area' element="Area" />
+            <Route path='/bar' element="Bar" />
+            <Route path='/pie' element="Pie" />
+            <Route path='/financial' element="Financial" />
+            <Route path='/color-mapping' element="ColorMapping" />
+            <Route path='/pyramid' element="Pyramid" />
+            <Route path='/stacked' element="Stacked" />
           </Routes>
          </div>
         </div>
